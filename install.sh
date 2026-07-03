748568d5711ff90531873cb74a3d6c6b1daf0cad
Om Talsania <omtalsania@geekypedia.net>
2026-07-03 23:24:39 +0530

    Added support for additional game engines

--- install.sh
+++ install.sh
@@ -2016,7 +2016,7 @@ EOF
         rm -f "$TMP_DEB"
     '
 
-    run_step "flare" "Flare RPG Game Engine" "command -v flare >/dev/null 2>&1'
+    run_step "flare" "Flare RPG Game Engine" "command -v flare >/dev/null 2>&1"'
         sudo apt install flare flare-game flare-engine
     '
     
